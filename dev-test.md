5228c72560371476f28be045d1c41c7ad54ba6b6
Kevin Miller <kmillergit@outlook.com>
2020-08-18 09:03:38 -0700

    updated error handling

--- dev-test.md
+++ dev-test.md
@@ -1,4 +1,4 @@
-# asfd
+# fdas
 
 ## Table of Contents
 
@@ -10,22 +10,22 @@
 6. <a href="#test">Tests</a>
 7. <a href="#questions">Issues and Questions</a>
 <h3 id='description'>Description</h3>
-afds
+fads
 
 <h3 id='installation'>Installation
-asfd
+
 
 <h3 id='usage'>Usage
-asdf
+
 
 <h3 id='contributions'>Contributions
-asdf
+
 
 <h3 id='license'>License
 MIT
 
 <h3 id='test'>Tests
-fads
+
 
 <h3 id='questions'>Issues and Questions
 Issues and questions can be directed to . The author's GitHub profile may be found at https://github.com/.
--- index.js
+++ index.js
@@ -3,64 +3,37 @@ const fs = require("fs");
 const markdown = require("./generateMarkdown.js");
 const questions = require("./questions");
 
-// this function checkError is redundant with the catch function below
-// because the catch will already catch any errors from the writeToFile
 
+/** Error checking function for asynchronous callbacks */
 function checkError(error) {
-    // throw new Error ("OH NO"); 
     if (error) {
-        console.log(error + " was the writeFile error");
+        return console.log(error + " was the asynchronous callback error");
     }
-    else {
-        console.log("success");
-    }
+    return console.log("success");
 }
 
 
-// function to write README file
+/** function to write README file */
 function writeToFile(fileName, data) {
-
     let readmeString = markdown.generateMarkdown(data);
-    try {
-        fs.writeFileSync(fileName, readmeString, function(error) {
-            checkError(error)
-        })
-    }
-    catch (error) {
-        console.log(error + " error was caught")
-    }
-
+    fs.writeFile(fileName, readmeString, error => checkError(error));
 }
 
 
-// function to initialize program
+/** function to initialize program */
 function init() {
     inquirer.prompt(questions.questions).then(answers => {
-        console.log(answers)
+        console.log(answers);
+        // The following try / catch block will only catch errors from generateMarkdown(), and not the callback checkError().
+        // Due to the asynchronous nature of the callback, callback will run after the try / catch block is already complete.
         try {
             writeToFile(answers.fileName, answers);
         }
-        // I think the reason this catch won't catch the error from checkError is because of the asynchronous nature of writeFile.
-        // using writeFileSync won't call checkError, because there is no need for a callback.
-        // using the async writeFile will produce the thrown error, but it won't be caught here because this code
-        // runs BEFORE the error is thrown - asychnchronous!!!! In other words, the catch isn't part of the promise!!
         catch (error) {
-            console.log(error + " error was caught")
+            console.log(error + " error was caught");
         }
     });
 }
 
 // function call to initialize program
 init();
-
-
-
-
-
-    // Alternate way of error handling
-    // fs.writeFile(fileName, JSON.stringify(data) + "\n", function(error) {
-    //     if (error) {
-    //         return console.log(error);
-    //     }
-    // })
-    // console.log("success");
